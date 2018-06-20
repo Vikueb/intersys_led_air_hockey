76fc8635799add9cda490783b0230648238dbe29
Vikueb <artemis0597@yahoo.de>
2018-06-20 19:40:09 +0200

    first steps with line finding and matrix initialisation

---
+++ src/Matrix.py
@@ -0,0 +1,38 @@
+import random as random
+
+
+class Pixel:
+    x = 0
+    y = 0
+
+    def __init__(self, x_value, y_value):
+        self.x = x_value
+        self.y = y_value
+
+
+class Matrix:
+    # nodes define a 32 times 64 array array where x_Max is 32 and y_Max is 64
+    x_Max = 32
+    y_Max = 64
+    board = []
+    goal_player1 = []
+    goal_player2 = []
+
+    def __init__(self):
+        self.board = self.field()
+        x_start = random.randint(16, 17)
+        y_start = random.randint(32, 33)
+        self.start = Pixel(x_start, y_start)
+
+        return
+
+    def field(self):
+        field = []
+        for y in range(self.y_Max):
+            new_line = []
+            for x in range(self.x_Max):
+                new_line.append((Pixel(x, y)))
+
+            field.append(new_line)
+
+        return field
---
+++ src/print.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = []
+for p in np.arange(10):
+    p *= -1
+    a.append(p)
+
+print(a)
