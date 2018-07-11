989d5a903093ec15b0510c4a57970f3466cf9d59
Vincent Kübler <s8vikueb@stud.uni-saarland.de>
2018-07-11 15:00:44 +0200

    bug fixing

--- src/Game.py
+++ src/Game.py
@@ -147,8 +147,8 @@ def take_and_process_picture():
 
     print(type(left_hand), type(right_hand))
 
-    left_hand = cv2.fromarray(left_hand)
-    right_hand = cv2.fromarray(right_hand)
+    left_hand = cv2.cv.fromarray(left_hand)
+    right_hand = cv2.cv.fromarray(right_hand)
 
     left_contours = cv2.findContours(left_hand, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
     right_contours = cv2.findContours(right_hand, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
