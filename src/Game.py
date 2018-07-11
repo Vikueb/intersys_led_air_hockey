1bff3d1d9cffd92717ae3bc3ee8473145214d7c9
Vincent Kübler <s8vikueb@stud.uni-saarland.de>
2018-07-11 15:23:16 +0200

    bug fixing

--- src/Game.py
+++ src/Game.py
@@ -104,7 +104,9 @@ def take_and_process_picture():
     camera.capture(stream, format="bgr", use_video_port=True)
     camera.stop_preview()
 
-    cv2.imshow("test", cv2.VideoCapture().read()[1])
+    frame = cv2.VideoCapture().read()[1]
+    cv2.imshow("test", frame)
+    print(frame.size, frame.type())
 
     # turn data into cv2 image
     # print(stream.array[0].size / 3, stream.array.size / stream.array[0].size)
