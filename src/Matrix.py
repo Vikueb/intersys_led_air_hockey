af3fbbcf9f2b263d9ea69c9ebc73eeffe22ee496
Vincent Kübler <s8vikueb@stud.uni-saarland.de>
2018-07-23 21:50:20 +0200

    fix

--- src/Matrix.py
+++ src/Matrix.py
@@ -46,15 +46,18 @@ class Matrix:
         # pins : [LAT, OE,  A,  B,  C,  D, R1, G1, B1, R2, G2, B2, clock]
         #        [  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11,    12]
         self.field[x][y] = string
-        for p in self.pins[4:7]:
-            GPIO.output(p, GPIO.HIGH)
 
+        GPIO.output(self.pins[2], GPIO.HIGH)
+        GPIO.output(self.pins[3], GPIO.HIGH)
+        GPIO.output(self.pins[4], GPIO.HIGH)
+        GPIO.output(self.pins[7], GPIO.HIGH)
         GPIO.output(self.pins[12], GPIO.HIGH)
 
-        for p in self.pins[4:7]:
-            GPIO.output(p, GPIO.LOW)
-
         GPIO.output(self.pins[12], GPIO.LOW)
+        GPIO.output(self.pins[7], GPIO.LOW)
+        GPIO.output(self.pins[4], GPIO.LOW)
+        GPIO.output(self.pins[3], GPIO.LOW)
+        GPIO.output(self.pins[2], GPIO.LOW)
 
         return
 
